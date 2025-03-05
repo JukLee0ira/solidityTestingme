233d882b8fce9c49281f0994c431bbcaf33d6545
JukLee0ira <meebookMonkey@163.com>
2025-03-05 17:01:34 +0800

    add devnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    // ... existing networks ...
+    devnet: {
+      url: "https://devnetstats.hashlabs.apothem.network/devnet:8545",
+    },
+  },
 };
 
 export default config;
