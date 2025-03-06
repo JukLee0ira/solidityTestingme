ab8680e6c6d5a60ecc0a3a3d3e5bde158bd424d3
JukLee0ira <meebookMonkey@163.com>
2025-03-06 14:40:14 +0800

    fix test  contract error

--- contracts/v0.8.24/YulFeatureTest.sol
+++ contracts/v0.8.24/YulFeatureTest.sol
@@ -2,28 +2,47 @@
 pragma solidity ^0.8.24;
 
 contract YulFeatureTest {
-    // Test Yul features
-    function testYulFeatures() public pure returns (bytes32, uint256, bytes32) {
-        bytes32 hash;
+    // 测试 blobbasefee()
+    function testBlobBaseFee() public view returns (uint256) {
         uint256 blobFee;
-        bytes32 copiedData;
-        
         assembly {
-            // Test blobbasefee()
             blobFee := blobbasefee()
-            
-            // Test blobhash()
+        }
+        return blobFee;
+    }
+
+    // 测试 blobhash()
+    function testBlobHash() public view returns (bytes32) {
+        bytes32 hash;
+        assembly {
             hash := blobhash(0)
-            
-            // Test mcopy()
+        }
+        return hash;
+    }
+
+    // 测试 mcopy()
+    function testMcopy() public pure returns (bytes32) {
+        bytes32 copiedData;
+        assembly {
             let ptr := mload(0x40)
             mcopy(ptr, 0x00, 32)
             copiedData := mload(ptr)
-            
-            // Test tload() and tstore()
-            tstore(0, 0x1234)
-            let storedValue := tload(0)
         }
-        return (hash, blobFee, copiedData);
+        return copiedData;
+    }
+
+    // 测试 tload() 和 tstore()
+    function testTstore() public {
+        assembly {
+            tstore(0, 0x1234)
+        }
+    }
+
+     function testTload() public view returns (uint256) {
+        uint256 storedValue;
+        assembly {
+            storedValue := tload(0)
+        }
+        return storedValue;
     }
 } 
