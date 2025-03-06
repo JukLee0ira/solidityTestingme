47fcb4d453f933a20ee14d6f38d9b12b38ec084f
JukLee0ira <meebookMonkey@163.com>
2025-03-06 16:11:07 +0800

    fix hardhat version

--- scripts/deploy_v24.ts
+++ scripts/deploy_v24.ts
@@ -1,14 +1,35 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const BlobFeatureTest = await ethers.getContractFactory(
-    "contracts/v0.8.24/BlobFeatureTest.sol"
-  );
-
+  // 部署BlobFeatureTest
+  const BlobFeatureTest = await ethers.getContractFactory("BlobFeatureTest");
   const blobFeatureTest = await BlobFeatureTest.deploy();
   await blobFeatureTest.deployed();
-
   console.log(`BlobFeatureTest deployed to ${blobFeatureTest.address}`);
+
+  // 部署BytesConcatTest
+  const BytesConcatTest = await ethers.getContractFactory(
+    "contracts/v0.8.24/BytesConcatTest"
+  );
+  const bytesConcatTest = await BytesConcatTest.deploy();
+  await bytesConcatTest.deployed();
+  console.log(`BytesConcatTest deployed to ${bytesConcatTest.address}`);
+
+  // 部署FunctionPointerTest
+  const FunctionPointerTest = await ethers.getContractFactory(
+    "contracts/v0.8.24/FunctionPointerTest"
+  );
+  const functionPointerTest = await FunctionPointerTest.deploy();
+  await functionPointerTest.deployed();
+  console.log(`FunctionPointerTest deployed to ${functionPointerTest.address}`);
+
+  // 部署YulFeatureTest
+  const YulFeatureTest = await ethers.getContractFactory(
+    "contracts/v0.8.24/YulFeatureTest"
+  );
+  const yulFeatureTest = await YulFeatureTest.deploy();
+  await yulFeatureTest.deployed();
+  console.log(`YulFeatureTest deployed to ${yulFeatureTest.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
