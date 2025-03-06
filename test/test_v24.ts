e889933c0c42b7280f64970942c53307980a5def
JukLee0ira <meebookMonkey@163.com>
2025-03-06 15:30:13 +0800

    use ether v5

--- test/test_v24.ts
+++ test/test_v24.ts
@@ -1,5 +1,5 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";TODO: need to rm
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
