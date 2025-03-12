ccaf67dc6210ec52aa343bb50268cfa5ac21da0e
JukLee0ira <meebookMonkey@163.com>
2025-03-13 00:15:57 +0800

    add comment to contract

--- contracts/v0.8.27/RequireWithErrorTest.sol
+++ contracts/v0.8.27/RequireWithErrorTest.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 contract RequireWithErrorTest {
     error InvalidValue(uint256 value);
     error UnauthorizedAccess(address caller);
-    //Tesing require(bool, Error) available when using the legacy pipeline
+    // Testing `require(bool, Error)`: Make require(bool, Error) available when using the legacy pipeline.
     function testRequireWithError(uint256 value) public pure {
         require(value > 100, InvalidValue(value));
     }
